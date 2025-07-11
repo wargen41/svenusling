89fd83bcaeb4efebc205753b249c82476cd52824
wargen41 <106013425+wargen41@users.noreply.github.com>
2025-07-11 20:45:36 +0200

    Update index.php

--- site/index.php
+++ site/index.php
@@ -4,24 +4,44 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Titel</title>
+        <title>svenusling</title>
         <style type="text/css">
         body {
-            margin:40px auto;
-            max-width:650px;
-            line-height:1.6;
-            font-size:18px;
-            color:#444;
-            padding:0 10px;
+            margin: 40px auto;
+            max-width: 650px;
+            line-height: 1.6;
+            font-size: 18px;
+            /* https://github.com/system-fonts/modern-font-stacks#humanist */
+            font-family: Seravek, 'Gill Sans Nova', Ubuntu, Calibri, 'DejaVu Sans', source-sans-pro, sans-serif;
+            color: #444;
+            padding: 0 10px;
         }
         h1,h2,h3 {
-            line-height:1.2
+            line-height: 1.2;
+            /* https://github.com/system-fonts/modern-font-stacks#geometric-humanist */
+            font-family: Avenir, Montserrat, Corbel, 'URW Gothic', source-sans-pro, sans-serif;
         }
         </style>
     </head>
     <body>
 <?php require 'templates/header.php';?>
         <h1>Rubrik</h1>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ornare magna sit amet lectus lacinia rhoncus vestibulum sit amet velit. Cras eget accumsan massa. Suspendisse ex velit, imperdiet a urna quis, mattis consequat nulla. Sed a magna eu tellus tempor sodales nec sit amet felis. In non orci nec elit sodales porttitor. Cras enim purus, egestas ac ex eget, sodales laoreet lacus. Pellentesque fermentum, ex in molestie elementum, erat orci commodo felis, vel pharetra erat tellus at tortor. Donec ut lectus vitae massa scelerisque euismod. Mauris nisl massa, interdum luctus nunc et, aliquam laoreet metus. Maecenas fermentum eget tellus non tempor. Nulla suscipit velit dui, sed luctus massa fermentum vitae. Maecenas vehicula sodales vulputate. Quisque vel leo risus. Maecenas enim sem, porttitor vitae magna nec, elementum maximus est.
+        </p>
+        <p>
+        Donec nulla urna, pellentesque in arcu a, varius porttitor est. Curabitur ut mi ut tortor ultrices mattis. Proin scelerisque lacinia risus nec rutrum. Quisque accumsan orci ac dictum varius. Aenean at eros at ex vestibulum auctor. Integer lacinia at nisl id vehicula. Proin in finibus nisl. Integer aliquet a metus a porttitor. Quisque sed eros a purus porta egestas. Integer non sagittis tortor. In hac habitasse platea dictumst. Mauris consectetur mi quis lorem lobortis gravida. Morbi sed leo sem. Pellentesque tincidunt erat in arcu ornare, quis tincidunt est ornare.
+        </p>
+        <h2>Underrubrik</h2>
+        <p>
+        Proin ullamcorper bibendum interdum. Maecenas porta ligula eu tortor hendrerit ornare. Cras commodo enim lectus, vitae lobortis ante semper vel. Pellentesque et posuere diam. Fusce eleifend sollicitudin suscipit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ex sapien, tempus in eros sagittis, dignissim posuere nunc. In posuere rutrum augue sit amet sagittis. Sed auctor vulputate felis vel porttitor. Nam neque justo, maximus vel vulputate in, eleifend eget tellus. Maecenas iaculis dignissim cursus. Quisque tempor pretium lorem eu viverra.
+        </p>
+        <p>
+        Cras quis fringilla magna. Nam consectetur odio sed sagittis finibus. Aliquam ullamcorper eu lectus ut scelerisque. Suspendisse non mattis nibh. Nunc orci dolor, pharetra sed mauris eu, rutrum pharetra velit. In eget rhoncus augue, ut congue massa. Aliquam pulvinar quam sollicitudin ipsum fermentum, id fringilla neque tempor.
+        </p>
+        <p>
+        Morbi ante lorem, scelerisque nec massa sit amet, malesuada vulputate ipsum. In tristique, justo id imperdiet maximus, purus tortor dapibus magna, in blandit purus eros vel lacus. Sed lectus neque, interdum non vehicula at, pretium nec odio. Etiam hendrerit tempus mattis. Donec at leo mattis, luctus lectus a, facilisis ipsum. Curabitur volutpat nunc vitae tempor euismod. Mauris sed nisl euismod, mattis lectus nec, faucibus nisl. Suspendisse hendrerit ut urna non suscipit. Maecenas malesuada lacus sapien. Fusce laoreet mauris turpis, non dapibus felis porta vel. Mauris quis fringilla diam.
+        </p>
 <?php require 'templates/footer.php';?>
     </body>
 </html>
