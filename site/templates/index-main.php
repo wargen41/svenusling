c639298a41feed135fffaf496f4ceb1c239075a9
wargen41 <106013425+wargen41@users.noreply.github.com>
2025-07-17 12:03:48 +0200

    Update index-main.php

--- site/templates/index-main.php
+++ site/templates/index-main.php
@@ -1,4 +1,41 @@
 <main id="main-content">
+
+<h2>Citattest</h2>
+<p>Mal</p>
+<blockquote>
+<p>This here's all part of our new plan!</p>
+</blockquote>
+<pre><code>Mal walks away
+</code></pre>
+<p>Kaylee</p>
+<blockquote>
+<p>Captain, how exactly is this part of our...</p>
+</blockquote>
+<p>Mal</p>
+<blockquote>
+<p>Still workin' the details.</p>
+</blockquote>
+
+<h2>Markdown-koden</h2>
+<pre>
+Mal
+
+> This here's all part of our new plan!
+
+```
+Mal walks away
+```
+
+Kaylee
+
+> Captain, how exactly is this part of our...
+
+
+Mal
+
+> Still workin' the details.
+</pre>
+
 <?php dynamicArticle('INTRODUCTION'); ?>
 <?php includeModule('puff-rated-movies.php'); ?>
 <?php includeModule('puff-video-reviews.php'); ?>
