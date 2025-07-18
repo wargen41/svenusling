11d393754d20fa6239c21cabe3bb07895e7ea436
wargen41 <106013425+wargen41@users.noreply.github.com>
2025-07-18 08:36:46 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,10 @@
 <?php
-// If the site is in a sub directory, write it with a starting slash
-// Otherwise leave this value blank
+// BASE URI (use one, comment out the other)
+// If the site is in a sub directory, 
+// write it with a starting slash, like this:
 $GLOBALS['base_uri'] = "/svenusling-site";
+// Otherwise leave this value blank, like this:
+// $GLOBALS['base_uri'] = "";
 
 // Full path to the SQLite database file
 $GLOBALS['db_path'] = "/var/www/db/svenusling/";
