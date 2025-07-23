6ac3063254971de533d2ba4447f7c600a178d321
wargen41 <106013425+wargen41@users.noreply.github.com>
2025-07-23 11:23:18 +0200

    Update site-widget.php

--- site/includes/templates/site-widget.php
+++ site/includes/templates/site-widget.php
@@ -1,4 +1,6 @@
-<span id="site-name"><?php echo $my_site['name'] ?></span>
+<a href="<?php echo $GLOBALS['base_uri']; ?>"><span id="site-name"><?php echo $my_site['name'] ?></span></a>
 <div id="site-widget">
-<a href="<?php echo $GLOBALS['base_uri'] ?>" title="<?php echo getStr('MAIN_MENU_TITLE'); ?>"><img class="circle" alt="<?php echo getStr('MAIN_MENU_TITLE'); ?>" src="<?php echo $GLOBALS['base_uri'] . $my_site['icon'] ?>"></a>
+    <a href="" title="<?php echo getStr('MAIN_MENU_TITLE'); ?>">
+        <img class="circle" alt="<?php echo getStr('MAIN_MENU_TITLE'); ?>" src="<?php echo $GLOBALS['base_uri'] . $my_site['icon'] ?>">
+    </a>
 </div>
