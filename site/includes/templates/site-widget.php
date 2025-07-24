7a11661c61624e8625042ffee8be19cc602c7d54
wargen41 <106013425+wargen41@users.noreply.github.com>
2025-07-24 12:23:23 +0200

    Menu button experiments

--- site/includes/templates/site-widget.php
+++ site/includes/templates/site-widget.php
@@ -1,6 +1,20 @@
-<a href="<?php echo $GLOBALS['base_uri'].'/'; ?>"><span id="site-name"><?php echo $my_site['name'] ?></span></a>
-<div id="site-widget">
-    <a href="" title="<?php echo getStr('MAIN_MENU_TITLE'); ?>">
+<!--<a href="<?php echo $GLOBALS['base_uri'].'/'; ?>"><label for="site-widget" id="site-widget-label"><?php echo $my_site['name'] ?></label></a>-->
+
+<label for="site-widget-button" id="site-widget-label"><?php echo getStr('MAIN_MENU_TITLE'); ?></label><div id="site-widget">
+<button id="site-widget-button" onclick="javascript:location.href='<?php echo $GLOBALS['base_uri'].'/'; ?>'" title="<?php echo getStr('MAIN_MENU_TITLE'); ?>">
+    <img class="circle" alt="<?php echo getStr('MAIN_MENU_TITLE'); ?>" src="<?php echo $GLOBALS['base_uri'] . $my_site['icon'] ?>">
+</button>
+</div>
+
+<!-- Försök med input-element (cirkeln blir inte rund?!?!)
+<label for="site-widget-button" id="site-widget-label"><?php echo getStr('MAIN_MENU_TITLE'); ?></label><div id="site-widget">
+    <input class="circle" type="image" src="<?php echo $GLOBALS['base_uri'].$my_site['icon'] ?>" alt="<?php echo getStr('MAIN_MENU_TITLE'); ?>" id="site-widget-button" onclick="javascript:location.href='<?php echo $GLOBALS['base_uri'].'/'; ?>'" title="<?php echo getStr('MAIN_MENU_TITLE'); ?>">
+    </a>
+</div>-->
+
+<!-- backup-kod som funkar med a-element istället för button
+<label for="site-widget-button" id="site-widget-label"><?php echo getStr('MAIN_MENU_TITLE'); ?></label><div id="site-widget">
+    <a id="site-widget-button" onclick="javascript:location.href='<?php echo $GLOBALS['base_uri'].'/'; ?>'" title="<?php echo getStr('MAIN_MENU_TITLE'); ?>">
         <img class="circle" alt="<?php echo getStr('MAIN_MENU_TITLE'); ?>" src="<?php echo $GLOBALS['base_uri'] . $my_site['icon'] ?>">
     </a>
-</div>
+</div>-->
---
+++ site/styles/css-base.php
@@ -0,0 +1,181 @@
+<style>
+:root {
+    --background-color-default: white;
+    --background-color-inverted: #333;
+    --text-color-default: #444;
+    --text-color-inverted: white;
+}
+html {
+    scroll-behavior: smooth;
+}
+body {
+    margin: 0 0 4rem 0;
+    line-height: 1.6;
+    font-size: 18px;
+    /* https://github.com/system-fonts/modern-font-stacks#humanist */
+    font-family: Seravek, 'Gill Sans Nova', Ubuntu, Calibri, 'DejaVu Sans', source-sans-pro, sans-serif;
+    color: var(--text-color-default);
+    padding: 0;
+    /* Positioning the site-widget */
+    text-align: right;
+}
+body * {
+    /* Default text alignment */
+    text-align: left;
+}
+h1, h2, h3, h4, h5, h6 {
+    line-height: 1.2;
+    padding: .5rem 0;
+    text-wrap: balance;
+    /* https://github.com/system-fonts/modern-font-stacks#geometric-humanist */
+    font-family: Avenir, Montserrat, Corbel, 'URW Gothic', source-sans-pro, sans-serif;
+    /* https://github.com/system-fonts/modern-font-stacks#didone */
+    /*font-family: Didot, 'Bodoni MT', 'Noto Serif Display', 'URW Palladio L', P052, Sylfaen, serif;*/
+    /* https://github.com/system-fonts/modern-font-stacks#antique */
+    /*font-family: Superclarendon, 'Bookman Old Style', 'URW Bookman', 'URW Bookman L', 'Georgia Pro', Georgia, serif;*/
+}
+h1 {
+    font-size: 200%;
+}
+main {
+    max-width: 36em;
+    margin: 1rem auto;
+    padding: 0 1.5rem;
+}
+main h1 {
+    text-align: center;
+    position: sticky;
+    top: 0;
+    background: var(--background-color-default);
+}
+p {
+    text-align: left;
+}
+a,
+details > summary,
+label[for]:not([for=""])
+{
+    cursor: pointer;
+}
+details > summary > * {
+    display: inline-block;
+}
+.skip-link {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    text-align: center;
+    background-color: var(--background-color-inverted);
+    color: var(--text-color-inverted);
+    padding: .5rem;
+    translate: 0 -100%;
+    transition: translate 150ms ease-in-out;
+}
+.skip-link:focus {
+    translate: 0;
+    z-index: 999;
+}
+img.circle
+{
+    border-radius: 50%;
+    object-fit: cover; /* Ensures the image fills the circle without distortion */
+}
+footer {
+    position: relative;
+    width: auto;
+    display: flex;
+    align-items: center;
+    justify-content: end;
+    gap: 1em;
+    flex-direction: row;
+
+    padding: 1rem 0;
+    margin-top: 5rem;
+
+    background: var(--background-color-inverted);
+    color: var(--text-color-inverted);
+}
+footer a {
+    color: var(--text-color-inverted);
+}
+header {
+    position: relative;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 3rem;
+    flex-direction: row;
+
+    background: var(--background-color-inverted);
+    color: var(--text-color-inverted);
+}
+header a {
+    color: var(--text-color-inverted);
+}
+nav ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 1rem 0;
+    line-height: 0.7;
+    display: flex;
+    gap: 1rem;
+    flex-direction: row;
+    flex-wrap: wrap;
+}
+nav ul > li {
+    padding: .5rem;
+}
+#site-widget {
+position: sticky;
+z-index: 999;
+top: 0;
+display: inline-block;
+margin: 0 0 0 0;
+padding: 0 0 0 0;
+}
+/* Test med input[type=button] (funkade inte, testar button istället) */
+/*#site-widget input:first-of-type
+{
+width: 2.2rem;
+height: 2.2rem;
+}
+#site-widget input:first-of-type
+{
+display: inline-block;
+padding: .7rem 1rem 1rem .5rem;
+}*/
+/* Backup-CSS som funkar med a-element innan byte till button */
+/*#site-widget img:first-of-type,
+#site-widget a:first-of-type
+{
+    width: 2.2rem;
+    height: 2.2rem;
+}
+#site-widget a:first-of-type
+{
+    display: inline-block;
+    padding: .7rem 1rem 1rem .5rem;
+}*/
+/* Försök med button istället */
+#site-widget img:first-of-type,
+#site-widget button:first-of-type
+{
+    width: 2.2rem;
+    height: 2.2rem;
+}
+#site-widget button:first-of-type
+{
+    display: inline-block;
+    background-color: #04AA6D;
+    border: none;
+    padding: .7rem 1rem 1rem .5rem;
+}
+
+#site-widget-label {
+height: 2.2rem;
+display: inline-block;
+transform: translateY(-30%)
+}
+</style>
