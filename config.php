443c9a991a034ca100c36abf61f1d9b6b4e6dff0
wargen41 <106013425+wargen41@users.noreply.github.com>
2025-07-24 09:29:15 +0200

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,12 @@ $GLOBALS['base_uri'] = "/svenusling-site";
 // Otherwise leave this value blank, like this:
 // $GLOBALS['base_uri'] = "";
 
-// Full path to the SQLite database file
+// Path to the SQLite database file
+// There is no need to put the database
+// in a directory with web access
+// Just make sure that the web server user
+// has read/write access to the file as
+// well as the parent directory
 $GLOBALS['db_path'] = "/var/www/db/svenusling/";
 $GLOBALS['db_name'] = "svenusling.db";
 ?>
