15243c9ce2d893bb1965d1729f74843bfe7ab20c
wargen41 <106013425+wargen41@users.noreply.github.com>
2025-08-23 16:37:35 +0200

    Update connections.php

--- site/admin/edit/movie/connections.php
+++ site/admin/edit/movie/connections.php
@@ -1,6 +1,6 @@
 <?php
 $page_url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-$location = $page_url;
+$location = removeQueryFromURL($page_url, 'section');
 echo htmlInput(array(
     "attributes" => array(
         "type" => "hidden",
