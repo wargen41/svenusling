5b8f461dd764c434f8bf97484aa2173bb4931480
wargen41 <106013425+wargen41@users.noreply.github.com>
2025-09-05 10:33:30 +0000

    Update intro.php

--- site/view/movies/intro.php
+++ site/view/movies/intro.php
@@ -6,4 +6,4 @@ includeModule('puff-rated-movies.php');
 
 ?>
 
-<p><a href="<?php echo $GLOBALS['base_uri'].'/view/movies/?list=all' ?>">Se hela listan</a></p>
+<p><a href="<?php echo $GLOBALS['base_uri'].'/view/movies/?list=films' ?>">Se hela listan</a></p>
