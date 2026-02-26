913406666e6af25c90bcf823a41f0fec7a7e00f4
wargen41 <106013425+wargen41@users.noreply.github.com>
2026-02-26 21:29:20 +0100

    Create deploy-test-svenusling.sh

---
+++ deploy-test-svenusling.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This is to be run via SSH on the web server
+
+# Check if the domain argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <domain>"
+  exit 1
+fi
+
+# Assign the first argument to the domain variable
+domain=$1
+
+# Navigate to the domain directory
+cd ~/domains/"$domain" || { echo "Error: The domain '$domain' does not exist."; exit 1; }
+
+# Download the zip file
+wget https://github.com/wargen41/svenusling/archive/main.zip
+
+# Unzip files in the site directory
+unzip main.zip "svenusling-main/api/test/*"
+
+# Create backup copy of public_html
+tar -czvf public_html-backup.tar.gz public_html
+
+# Move files to public_html
+# Save backups of the earlier version
+rsync -av --backup --suffix=~ svenusling-main/api/test/ public_html/
+
+# Remove temporary directories and files
+rm main.zip
+rm -r svenusling-main
+
+echo "Done!"
